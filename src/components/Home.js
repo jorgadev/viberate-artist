2cf18a992079479afb85a8553ef9720d3a4baf7b
jorgadev <radovanjorgic14@gmail.com>
2021-04-19 22:36:26 -0700

    Home route changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Home() {
-  return <div>home</div>;
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", marginTop: 20 }}>Choose an artist..</h1>
+    </div>
+  );
 }
 
 export default Home;
